ff2e8208bdf1874dd9e54df94c52377660502cb5
FerielBn <feriel.bnsr@gmail.com>
2023-04-22 12:40:28 +0100

    'updated'

--- app/src/main/java/com/example/miniprojet/SignupActivity.java
+++ app/src/main/java/com/example/miniprojet/SignupActivity.java
@@ -167,7 +167,7 @@ public class SignupActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
