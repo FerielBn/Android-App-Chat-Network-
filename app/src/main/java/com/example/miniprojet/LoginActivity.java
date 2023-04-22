6b13a83d960b537dc9a0670e9da934d104cd4745
FerielBn <feriel.bnsr@gmail.com>
2023-04-22 12:28:33 +0100

    'updated'

---
+++ app/src/main/java/com/example/miniprojet/LoginActivity.java
@@ -0,0 +1,125 @@
+package com.example.miniprojet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://miniprojet-android-8900a-default-rtdb.firebaseio.com");
+    private EditText emailInput, passwordInput;
+    private Button signupBtn, loginBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initEvent();
+    }
+
+    private void initView() {
+        emailInput = findViewById(R.id.emailET);
+        passwordInput = findViewById(R.id.passwordET);
+        loginBtn = findViewById(R.id.loginBT);
+        signupBtn = findViewById(R.id.goSignupBT);
+    }
+
+    private void initEvent() {
+
+        SharedPreferences shared = getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shared.edit();
+        if(shared.getBoolean("logged",false)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = String.valueOf(emailInput.getText());
+                String password = String.valueOf(passwordInput.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // hello world
+                databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        boolean foundHim = false;
+                        for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                            String email2 = userSnapshot.child("email").getValue(String.class);
+                            String phone = userSnapshot.child("phone").getValue(String.class);
+                            String username = userSnapshot.child("username").getValue(String.class);
+                            String pass2 = userSnapshot.child("password").getValue(String.class);
+
+                            if (email2 != null && email2.equals(email)) {
+                                // i have found the email
+                                foundHim = true;
+                                if(pass2 != null && pass2.equals(password)){
+                                    Toast.makeText(LoginActivity.this, "Welcome User", Toast.LENGTH_SHORT).show();
+                                    editor.putString("email", email);
+                                    editor.putString("phone", phone);
+                                    editor.putString("username", username);
+                                    editor.putBoolean("logged", true);
+                                    editor.apply();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(LoginActivity.this, "Check Your Email/Password", Toast.LENGTH_SHORT).show();
+                                }
+                                break;
+                            }
+                        }
+                        if (!foundHim) {
+                            // There's no user with that email
+                            Toast.makeText(LoginActivity.this, "There's no user with that email ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
+
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/miniprojet/SignupActivity.java
@@ -0,0 +1,178 @@
+package com.example.miniprojet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SignupActivity extends AppCompatActivity {
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://miniprojet-android-8900a-default-rtdb.firebaseio.com");
+    private EditText usernameInput, emailInput,phoneInput, passwordInput,confPassInput;
+    private Button signupBtn, loginBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        initView();
+        initEvent();
+    }
+
+    private void initView() {
+
+        usernameInput = findViewById(R.id.usernameET);
+        emailInput = findViewById(R.id.emailET);
+        phoneInput = findViewById(R.id.phoneET);
+        passwordInput = findViewById(R.id.passwordET);
+        confPassInput = findViewById(R.id.confirmPasswordET);
+        loginBtn = findViewById(R.id.goLoginBT);
+        signupBtn = findViewById(R.id.signupBT);
+
+
+
+    }
+
+    private void initEvent() {
+        SharedPreferences shared = getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shared.edit();
+        if(shared.getBoolean("logged",false)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = String.valueOf(usernameInput.getText());
+                String email = String.valueOf(emailInput.getText());
+                String phone = String.valueOf(phoneInput.getText());
+                String password = String.valueOf(passwordInput.getText());
+                String confirm_password = String.valueOf(confPassInput.getText());
+
+                if(TextUtils.isEmpty(username)){
+                    Toast.makeText(SignupActivity.this, "Enter username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SignupActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(phone)){
+                    Toast.makeText(SignupActivity.this, "Enter phone number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(SignupActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirm_password)){
+                    Toast.makeText(SignupActivity.this, "password not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(!TextUtils.equals(password,confirm_password)){
+                    Toast.makeText(SignupActivity.this, "password not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // check if email exist already
+                        if(snapshot.hasChild(phone)){
+                            Toast.makeText(SignupActivity.this,"Phone Already used", Toast.LENGTH_SHORT).show();
+                            return;
+                        }else{
+
+                            databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    boolean emailExists = false;
+                                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                                        String email2 = userSnapshot.child("email").getValue(String.class);
+
+                                        if (email2 != null && email2.equals(email)) {
+                                            Toast.makeText(SignupActivity.this,"Email Already exist", Toast.LENGTH_SHORT).show();
+                                            emailExists = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!emailExists) {
+                                        databaseReference.child("users").child(phone).child("username").setValue(username);
+                                        databaseReference.child("users").child(phone).child("email").setValue(email);
+                                        databaseReference.child("users").child(phone).child("phone").setValue(phone);
+                                        databaseReference.child("users").child(phone).child("password").setValue(password);
+                                        editor.putString("email", email);
+                                        editor.putString("phone", phone);
+                                        editor.putString("username", username);
+                                        editor.putBoolean("logged", true);
+                                        editor.apply();
+                                        Toast.makeText(SignupActivity.this, "User Registered Successfuly", Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                }
+                            });
+
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+
+
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+    }
+
+}
