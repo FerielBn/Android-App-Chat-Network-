2a6a35959357b61be5a4b819e8c00135a7509579
hatem782 <hatembenechikh100@gmail.com>
2023-04-25 13:03:31 +0100

    starting new laytouts

--- app/src/main/java/com/example/miniprojet/MainActivity.java
+++ app/src/main/java/com/example/miniprojet/MainActivity.java
@@ -62,6 +62,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
                 break;
 
+            case R.id.nav_room:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyRoomFragment()).commit();
+                break;
+
             case R.id.nav_settings:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
                 break;
