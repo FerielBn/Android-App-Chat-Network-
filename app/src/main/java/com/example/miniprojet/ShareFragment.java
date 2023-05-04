e18180e000e03597504a589e383a20ec488cbc82
FerielBn <80517714+FerielBn@users.noreply.github.com>
2023-05-04 20:45:29 +0100

    Maps

--- app/src/main/java/com/example/miniprojet/ShareFragment.java
+++ app/src/main/java/com/example/miniprojet/ShareFragment.java
@@ -2,18 +2,53 @@ package com.example.miniprojet;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-public class ShareFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ShareFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false);
+        View view = inflater.inflate(R.layout.fragment_share, container, false);
+
+        map = view.findViewById(R.id.map);
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+
+        LatLng mapTunisia = new LatLng(33.8869, 9.5375);
+        this.gMap.addMarker(new MarkerOptions().position(mapTunisia));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapTunisia));
+
     }
 }
