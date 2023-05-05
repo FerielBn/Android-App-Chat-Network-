8c8017570d433573f144d56dec7c9bb9d35da9a6
FerielBn <feriel.bnsr@gmail.com>
2023-05-05 01:07:08 +0100

    maps

--- app/src/main/java/com/example/miniprojet/ShareFragment.java
+++ app/src/main/java/com/example/miniprojet/ShareFragment.java
@@ -1,27 +1,49 @@
 package com.example.miniprojet;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class ShareFragment extends Fragment implements OnMapReadyCallback {
+import java.util.List;
 
-    GoogleMap gMap;
-    FrameLayout map;
+
+public class ShareFragment extends Fragment{
+
+//    GoogleMap gMap;
+//    FrameLayout map;
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,26 +51,63 @@ public class ShareFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_share, container, false);
 
-        map = view.findViewById(R.id.map);
-
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+//        map = view.findViewById(R.id.map);
+//
+//        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 //        mapFragment.getMapAsync(this);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-
-        mapFragment.getMapAsync(this);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        Dexter.withContext(getContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        getCurrentLocation();
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+                        // handle permission denied case
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        // handle permission rationale case
+                    }
+                }).check();
 
 
         return view;
     }
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.gMap = googleMap;
+    public void getCurrentLocation(){
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
+                        if(location != null){
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("current location");
+                            googleMap.addMarker(markerOptions);
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                        }else{
+                            Toast.makeText(getContext(), "please turn on your location app permission", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-        LatLng mapTunisia = new LatLng(33.8869, 9.5375);
-        this.gMap.addMarker(new MarkerOptions().position(mapTunisia));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapTunisia));
+            }
+        });
 
     }
+
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//        this.gMap = googleMap;
+//
+//        LatLng mapTunisia = new LatLng(33.8869, 9.5375);
+//        this.gMap.addMarker(new MarkerOptions().position(mapTunisia));
+//        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapTunisia));
+//
+//    }
 }
